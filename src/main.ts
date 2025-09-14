125e940401557f848bcb07f854ec473e53ae0ff6
shuya-tamaru <tamaru1013@gmail.com>
2025-09-14 11:00:01 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import "./style.css";
 import { bootstrap } from "./app/bootstrap";
-const deviceType = getDeviceType();
 
-const count = deviceType === "mobile" ? 2000 : 10000;
-const defaultAspect = deviceType === "mobile" ? 9 / 16 : 16 / 9;
+window.addEventListener("load", () => {
+  const deviceType = getDeviceType();
+  const count = deviceType === "mobile" ? 2000 : 10000;
+  const defaultAspect = deviceType === "mobile" ? 9 / 16 : 16 / 9;
 
-bootstrap(count, defaultAspect);
+  bootstrap(count, defaultAspect);
+});
 
 export function getDeviceType(): "desktop" | "mobile" | "tablet" {
   const ua = navigator.userAgent.toLowerCase();
